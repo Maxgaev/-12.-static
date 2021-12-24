d49434d1b53abc2ca5c61c089d6493e2a051f20a
Максим Алехин <Max-crf@mail.ru>
2021-12-24 14:03:19 +0700

    Добавьте файлы проекта.

---
+++ Задание 12. Модификатор static/Points.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; 
+
+namespace Задание_12.Модификатор_static
+{
+ 
+        public static class Points
+        {
+            public static double l;
+            public static double S;
+
+            public static void Length(double r)
+            {
+                l = 2 * Math.PI * r;
+                Console.WriteLine("Длина окружности L = {0:00}", l);
+            }
+            public static void Square(double r)
+            {
+                S = Math.PI * r * r;
+                Console.WriteLine("Площадь круга S = {0:00}", S);
+            }
+            public static void Point(double x, double y, double r)
+            {
+                if (r >= Math.Sqrt(x * x + y * y))
+                {
+                    Console.WriteLine("Точка находится внутри круга");
+                }
+                else
+                {
+                    Console.WriteLine("Точка находится за пределами круга");
+                }
+            }
+        }
+    
+}
---
+++ Задание 12. Модификатор static/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Задание_12.Модификатор_static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите радиуc окружности:");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nВведите значение координаты X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nВведите значение координаты Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Points.Length(r);
+            Points.Square(r);
+            Points.Point(x, y, r);
+            Console.ReadKey();
+
+        }
+    
+
+    }
+}
